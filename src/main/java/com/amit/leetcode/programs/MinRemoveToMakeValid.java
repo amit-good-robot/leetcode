4abc62d710b460253e174064d47c3a817ccb218f
goodrobot403 <goodrobot403@gmail.com>
2024-07-11 21:39:11 +0100

    Leetcode problem solution

---
+++ src/main/java/com/amit/leetcode/programs/MinRemoveToMakeValid.java
@@ -0,0 +1,51 @@
+package com.amit.leetcode.programs;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+/**
+ * <a href="https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/description/">...</a>
+ */
+public class MinRemoveToMakeValid {
+
+    public static void main(String[] args) {
+        MinRemoveToMakeValid minRemoveToMakeValid = new MinRemoveToMakeValid();
+        String output = minRemoveToMakeValid.minRemoveToMakeValid("))((");
+        System.out.println("output : " + output);
+    }
+
+    public String minRemoveToMakeValid(String s) {
+
+        Stack<Integer> parenthesisStack = new Stack<>();
+        Set<Integer> indexToBeRemoved = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                parenthesisStack.push(i);
+            }
+            if (c == ')') {
+                if (!parenthesisStack.isEmpty()) {
+                    parenthesisStack.pop();
+                } else {
+                    indexToBeRemoved.add(i);
+                }
+            }
+        }
+
+        while (!parenthesisStack.isEmpty()) {
+            indexToBeRemoved.add(parenthesisStack.pop());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!indexToBeRemoved.contains(i)) {
+                sb.append(s.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
